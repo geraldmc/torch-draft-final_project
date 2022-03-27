3156f84a1a28212b5b62a041f6afaaa2dbd36415
geraldmc <gerald.mccollam@gmail.com>
2022-03-27 18:17:37 -0500

    ignore

--- conf/params.py
+++ conf/params.py
@@ -19,16 +19,17 @@ DEVICE = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 
 # define the other paths 
 
-GDRIVE_ZIP_SRC = '/content/gdrive/MyDrive/DL4CV-2022/project-II/data/images.zip'
+GD_ZIP_IMG = '/content/gdrive/MyDrive/DL4CV-2022/project-II/data/images.zip'
+GD_ZIP_MODEL = '/content/gdrive/MyDrive/DL4CV-2022/project-II/data/models.zip'
 IMG_DIRECTORY = '/content/torch-draft-final_project-main/data/images'
 IMG_ZIP_FILE = '/content/torch-draft-final_project-main/data/images.zip'
-IMG_GD_ID = '1dXykLwQ4wcHdI39YVc4rr6JFlo2ROSA_'
+MODEL_ZIP_FILE = '/content/torch-draft-final_project-main/data/models.zip'
 
 OUTPUT_PATH = 'output'
 LABEL_PATH = os.path.join(DATA_PATH, 'labels')
 MODEL_PATH = os.path.join(DATA_PATH, 'models')
 IMAGE_PATH = os.path.join(DATA_PATH, 'images')
-IMAGE_GD_ID = ''
 IMAGE_ZIP_FILE = os.path.join(IMAGE_PATH, 'images.zip')
 MODEL_GD_ID = ''
+IMAGE_GD_ID = ''
 YOLO_OUTPUT = os.path.join(OUTPUT_PATH, 'yolo_output')
