43abb60bfc94352d28f53da99194677ad1f4afdb
geraldmc <gerald.mccollam@gmail.com>
2022-04-06 17:12:14 -0500

    expand params

--- conf/params.py
+++ conf/params.py
@@ -2,10 +2,6 @@
 import torch
 import os
 
-# define the root data directory, train and test dataset paths
-DATA_PATH = 'data'
-TRAIN_PATH = os.path.join(DATA_PATH, 'train')
-TEST_PATH = os.path.join(DATA_PATH, 'test')
 
 # specify image size and batch size
 IMAGE_SIZE = 300
@@ -17,22 +13,42 @@ THRESHOLD = 0.50
 # determine the device type 
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 
-# other paths 
+# define the root data directory, train and test dataset paths
+DATA_PATH = 'data'
 
 GD_ZIP_IMG = '/content/gdrive/MyDrive/DL4CV-2022/project-II/data/images.zip'
 GD_ZIP_MODEL = '/content/gdrive/MyDrive/DL4CV-2022/project-II/data/models.zip'
 IMG_DIRECTORY = '/content/torch-draft-final_project-main/data/images'
-IMG_ZIP_FILE = '/content/torch-draft-final_project-main/data/images.zip'
-MODEL_DIRECTORY = '/content/torch-draft-final_project-main/data/models'
-MODEL_ZIP_FILE = '/content/torch-draft-final_project-main/data/models.zip'
-IMG_TRAIN_PATH = '/content/torch-draft-final_project-main/data/train'
 
 OUTPUT_PATH = 'output'
 LABEL_PATH = os.path.join(DATA_PATH, 'labels')
 MODEL_PATH = os.path.join(DATA_PATH, 'models')
 IMAGE_PATH = os.path.join(DATA_PATH, 'images')
+IMG_TRAIN_PATH = os.path.join(DATA_PATH, 'train')
+IMG_VAL_PATH = os.path.join(DATA_PATH, 'val')
+IMG_TEST_PATH = os.path.join(DATA_PATH, 'test')
 IMG_CLASSES_PATH = os.path.join(DATA_PATH, 'image_classes')
 IMAGE_ZIP_FILE = os.path.join(IMAGE_PATH, 'images.zip')
-MODEL_GD_ID = ''
-IMAGE_GD_ID = ''
 YOLO_OUTPUT = os.path.join(OUTPUT_PATH, 'yolo_output')
+
+# Global variables
+RAW_IMG_SIZE = (256, 256)
+IMG_SIZE = (224, 224)
+INPUT_SHAPE = (IMG_SIZE[0], IMG_SIZE[1], 3)
+MAX_EPOCH = 200
+BATCH_SIZE = 32
+FOLDS = 5
+STOPPING_PATIENCE = 32
+LR_PATIENCE = 16
+INITIAL_LR = 0.0001
+CLASSES = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+CLASS_NAMES = ['Chinee Apple',
+               'Lantana',
+               'Parkinsonia',
+               'Parthenium',
+               'Prickly Acacia',
+               'Rubber Vine',
+               'Siam Weed',
+               'Snake Weed',
+               'Negatives']
