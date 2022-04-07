066a66503f9058043af633f44b7cf560f258a0af
geraldmc <gerald.mccollam@gmail.com>
2022-04-07 16:54:12 -0500

    adds model

--- data/transforms.py
+++ data/transforms.py
@@ -2,6 +2,23 @@ import os
 
 import torchvision.transforms as transforms
 
+# Data augmentation and normalization for training
+# Just normalization for validation
+paired_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(input_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(input_size),
+        transforms.CenterCrop(input_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
 base_transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
