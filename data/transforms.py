a9970c5063b329c342021107801dc7a16d5a5148
geraldmc <gerald.mccollam@gmail.com>
2022-04-07 17:01:52 -0500

    adds model

--- data/transforms.py
+++ data/transforms.py
@@ -13,7 +13,7 @@ paired_transforms = {
     ]),
     'val': transforms.Compose([
         transforms.Resize(224),
-        transforms.CenterCrop(input_size),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
