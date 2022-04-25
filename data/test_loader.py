0626564f9f080936ddd84b6ba4cd84a5426a4af6
geraldmc <gerald.mccollam@gmail.com>
2022-04-25 09:08:45 -0500

    adds test data

--- data/test_loader.py
+++ data/test_loader.py
@@ -8,27 +8,25 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from PIL import Image
 
-from loader.transforms import base_transform
+from data.transforms import data_transforms
 
 # See: https://debuggercafe.com/custom-dataset-and-dataloader-in-pytorch/
 
 class DeepWeeds_Test(Dataset, csv_file):
 
-    def __init__(self, root_dir, test=True):
+    def __init__(self, test=True):
         """
         """
-        self.root_dir = root_dir
-        if test:
-            self.sub_directory = 'Final_Test/Images/'
-            self.csv_file_name = csv_file
-        self.transform = base_transform
+        self.sub_directory = 'data/test'
+        self.csv_file_name = csv_file
+        self.transform = data_transforms['default']
         self.frame = None
 
         csv_file_path = os.path.join(self.root_dir, 
                                      self.sub_directory, 
                                      self.csv_file_name)
         
-        self.csv_data = pd.read_csv(csv_file_path, sep=';')
+        self.csv_data = pd.read_csv(csv_file_path, sep=',')
 
     def __len__(self):
         return len(self.csv_data)
@@ -37,10 +35,8 @@ class DeepWeeds_Test(Dataset, csv_file):
         img_path = os.path.join(self.root_dir, 
                                 self.sub_directory,
                                 self.csv_data.iloc[idx, 0])
-        img_frame_coords = (    self.csv_data.iloc[idx, 3:7]) # Roi.X1, Roi.Y1, Roi.X2, Roi.Y2
         
         img = Image.open(img_path)
-        class_id = self.csv_data.iloc[idx, 7]
 
         if self.transform is not None:
             img = self.transform(img)
